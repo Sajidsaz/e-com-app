9d6352112aa011602f15b7a57569b5784076658d
Sajidsaz <sajidhsaz@gmail.com>
2026-04-24 23:54:21 +0530

    error check

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,8 +33,8 @@ const Login = () => {
         }
       }
     } catch (error) {
-      console.log(error);
-      toast.error(error.message);
+    console.log(error);
+    toast.error(error?.response?.data?.message || error.message);
     }
   }
 
